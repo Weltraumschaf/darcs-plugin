55c182cc657b9d2dab19a2230fb6ec56e5dec81d
Weltraumschaf <ich@weltraumschaf.de>
2011-02-02 22:36:07 +0100

    Added event handlers.

--- src/main/java/org/jvnet/hudson/plugins/darcs/DarcsSaxHandler.java
+++ src/main/java/org/jvnet/hudson/plugins/darcs/DarcsSaxHandler.java
@@ -5,6 +5,10 @@
 
 package org.jvnet.hudson.plugins.darcs;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
@@ -12,7 +16,37 @@ import org.xml.sax.helpers.DefaultHandler;
  * @author sxs
  */
 public class DarcsSaxHandler extends DefaultHandler {
+    private static final Logger logger = Logger.getLogger(DarcsSaxHandler.class.getName());
+    
     public DarcsSaxHandler() {
         super();
+    }
+
+    @Override
+    public void startDocument () {
+	logger.log(Level.INFO, "Start XML document");
+    }
+
+    @Override
+    public void endDocument () {
+	logger.log(Level.INFO, "End XML document");
+    }
+
+    @Override
+    public void startElement(String uri, String name, String qName, Attributes atts) {
+	if ("".equals (uri)) {
+	    logger.log(Level.INFO, "Start element: " + qName);
+	} else {
+	    logger.log(Level.INFO, "Start element: {" + uri + "}" + name);
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String name, String qName) {
+	if ("".equals (uri)) {
+	    logger.log(Level.INFO, "End element: " + qName);
+	} else {
+	    logger.log(Level.INFO, "End element:   {" + uri + "}" + name);
+        }
     }
 }
