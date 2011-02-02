5251182725288d8ee5f40341cae765cdbd29307b
Weltraumschaf <ich@weltraumschaf.de>
2011-02-02 22:11:32 +0100

    Added a SAX handler for parsing darcs changes xml.

--- src/main/java/org/jvnet/hudson/plugins/darcs/DarcsChangeLogParser.java
+++ src/main/java/org/jvnet/hudson/plugins/darcs/DarcsChangeLogParser.java
@@ -11,19 +11,37 @@ import hudson.scm.ChangeLogSet;
 import hudson.scm.ChangeLogSet.Entry;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileReader;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 /**
- *
+ * Parses the output of darcs log.
+ * 
  * @author Sven Strittmatter <ich@weltraumschaf.de>
  */
 public class DarcsChangeLogParser  extends ChangeLogParser {
 
     @Override
-    public ChangeLogSet<? extends Entry> parse(AbstractBuild ab, File file) throws IOException, SAXException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public ChangeLogSet<? extends Entry> parse(AbstractBuild build, File changelogFile) throws IOException, SAXException {
+        List<DarcsChangeSet> entries = new ArrayList<DarcsChangeSet>();
+
+        XMLReader xr = XMLReaderFactory.createXMLReader();
+        DarcsSaxHandler handler = new DarcsSaxHandler();
+	xr.setContentHandler(handler);
+	xr.setErrorHandler(handler);
+
+        FileReader r = new FileReader(changelogFile);
+        xr.parse(new InputSource(r));
+
+        return new DarcsChangeSetList(build, entries);
     }
 
 }
---
+++ src/main/java/org/jvnet/hudson/plugins/darcs/DarcsSaxHandler.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jvnet.hudson.plugins.darcs;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ *
+ * @author sxs
+ */
+public class DarcsSaxHandler extends DefaultHandler {
+    public DarcsSaxHandler() {
+        super();
+    }
+}
