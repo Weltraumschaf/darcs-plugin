1a6988f2d5dba1d58d0725689c0dfba85b3c1753
Weltraumschaf <ich@weltraumschaf.de>
2011-02-05 09:43:13 +0100

    Added license.

--- src/main/java/org/jvnet/hudson/plugins/darcs/DarcsChangeSetList.java
+++ src/main/java/org/jvnet/hudson/plugins/darcs/DarcsChangeSetList.java
@@ -1,6 +1,11 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * "Sven Strittmatter" <ich@weltraumschaf.de> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
  */
 
 package org.jvnet.hudson.plugins.darcs;
