4101d29eba9bb437536e58dedc722d042f67235d
Sven Strittmatter <ich@weltraumschaf.de>
2011-02-10 18:05:32 +0100

    Simplified to the minimum for debugging

--- src/main/java/org/jvnet/hudson/plugins/darcs/browsers/DarcsWeb.java
+++ src/main/java/org/jvnet/hudson/plugins/darcs/browsers/DarcsWeb.java
@@ -11,154 +11,45 @@
 package org.jvnet.hudson.plugins.darcs.browsers;
 
 import org.jvnet.hudson.plugins.darcs.DarcsChangeSet;
-import org.jvnet.hudson.plugins.darcs.DarcsChangeSet.Path;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.regex.Pattern;
-import javax.servlet.ServletException;
+import java.net.MalformedURLException;
 
 import hudson.Extension;
-import hudson.Util;
 import hudson.model.Descriptor;
 import hudson.scm.RepositoryBrowser;
-import hudson.util.FormValidation;
-
-import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.QueryParameter;
 
 /**
  *
  * @author Sven Strittmatter <ich@weltraumschaf.de>
  */
 public class DarcsWeb extends DarcsRepositoryBrowser {
-    
-    public final URL url;
-//    private final String repo;
-//
-//    @DataBoundConstructor
-//    public DarcsWeb(URL url, String repo) throws MalformedURLException {
-//        this.url  = normalizeToEndWithSlash(url);
-//        this.repo = repo;
-//    }
-
-    @DataBoundConstructor
-    public DarcsWeb(URL url) {
-        this.url = normalizeToEndWithSlash(url);
-    }
-    
-//
-//    public DarcsWeb(String url, String repo) throws MalformedURLException {
-//        this(new URL(url), repo);
-//    }
-//
-//    public URL getUrl() {
-//        return url;
-//    }
-//
-//    public String getRepo() {
-//        return repo;
-//    }
-//
-//    private String getBaseUrlString() {
-//        return String.format("%scgi-bin/darcsweb.cgi", this.url);
-//    }
-
-    public URL getChangeSetLink(DarcsChangeSet changeSet) throws IOException {
-//        return new URL(String.format("%s?r=%s;a=annotate_shade;h=%",
-//                                     getBaseUrlString(), this.repo, changeSet.getHash()));
-        return new URL("");
-    }
-
-    public URL getFileLink(DarcsChangeSet.Path file) throws IOException {
-        return new URL("");
-    }
-
-    /**
-     * Determines the link to the diff between the version
-     * in the specified revision of {@link GitChangeSet.Path} to its previous version.
-     *
-     * @param path affected file path
-     * @return
-     *      null if the browser doesn't have any URL for diff.
-     * @throws IOException
-     */
-//    public URL getDiffLink(DarcsChangeSet.Path path) throws IOException {
-//        if (path.getEditType().equals(EditType.DELETE)) {
-//            return getDiffLinkRegardlessOfEditType(path);
-//        } else {
-//            final String spec = "blob/" + path.getChangeSet().getId() + "/" + path.getPath();
-//            return new URL(url, url.getPath() /*+ spec*/);
-//            return new URL("");
-//        }
-//    }
-
-    /**
-     * Determines the link to a single file under Git.
-     * This page should display all the past revisions of this file, etc.
-     *
-     * @param path affected file path
-     * @return
-     *      null if the browser doesn't have any suitable URL.
-     * @throws IOException
-     */
-//    public URL getFileLink(DarcsChangeSet.Path path) throws IOException {
-//        if (path.getEditType() != EditType.EDIT) { return null; }
-//
-//        if (path.getSrc() == null) { return null; }
-//
-//        if (path.getDst() == null) { return null; }
-//
-//        return getDiffLinkRegardlessOfEditType(path);
-//    }
-
-    /**
-     * Return a diff link regardless of the edit type by appending the index of the pathname in the changeset.
-     *
-     * @param path
-     * @return
-     * @throws IOException
-     */
-//    private URL getDiffLinkRegardlessOfEditType(Path path) throws IOException {
-//        final DarcsChangeSet changeSet = path.getChangeSet();
-//        final ArrayList<String> affectedPaths = new ArrayList<String>(changeSet.getAffectedPaths());
-//        // Github seems to sort the output alphabetically by the path.
-//        Collections.sort(affectedPaths);
-//        final String pathAsString = path.getPath();
-//        final int i = Collections.binarySearch(affectedPaths, pathAsString);
-//        assert i >= 0;
-//        return new URL(getChangeSetLink(changeSet), "#diff-" + String.valueOf(i));
-//        return new URL("");
-//    }
-
     @Extension
     public static class DescriptorImpl extends Descriptor<RepositoryBrowser<?>> {
-
-        @Override
         public String getDisplayName() {
             return "Darcsweb";
         }
-
-        public FormValidation doCheck(@QueryParameter final String value) throws IOException, ServletException {
-            return new FormValidation.URLCheck() {
-                @Override
-                protected FormValidation check() throws IOException, ServletException {
-                    
-                    return FormValidation.ok();
-                }
-            }.check();
-        }
-
-        @Override
-        public Darcsden newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-            return req.bindParameters(Darcsden.class, "darcsweb.darcs.");
-        }
-
-        private static final Pattern URL_PATTERN = Pattern.compile(".+/cgi-bin/darcsweb.cgi");
     }
 
+
     private static final long serialVersionUID = 1L;
+
+    public final URL url;
+
+	@DataBoundConstructor
+    public DarcsWeb(URL url) throws MalformedURLException {
+        this.url = normalizeToEndWithSlash(url);
+    }
+
+    public URL getChangeSetLink(DarcsChangeSet changeSet) throws IOException {
+        return new URL("");
+    }
+
+
+    public URL getFileLink(DarcsChangeSet.Path path) throws IOException {
+		return new URL("");
+    }
 }
--- src/main/java/org/jvnet/hudson/plugins/darcs/browsers/Darcsden.java
+++ src/main/java/org/jvnet/hudson/plugins/darcs/browsers/Darcsden.java
@@ -14,87 +14,42 @@ import org.jvnet.hudson.plugins.darcs.DarcsChangeSet;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.regex.Pattern;
-import javax.servlet.ServletException;
+import java.net.MalformedURLException;
 
 import hudson.Extension;
-import hudson.Util;
 import hudson.model.Descriptor;
 import hudson.scm.RepositoryBrowser;
-import hudson.util.FormValidation;
-
-import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.QueryParameter;
 
 /**
  *
  * @author Sven Strittmatter <ich@weltraumschaf.de>
  */
-public class Darcsden extends DarcsRepositoryBrowser {
-    public final URL url;
-
-    @DataBoundConstructor
-    public Darcsden(URL url) {
-        this.url = normalizeToEndWithSlash(url);
-    }
-    
-    public URL getChangeSetLink(DarcsChangeSet e) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public URL getFileLink(DarcsChangeSet.Path file) throws IOException {
-        return new URL("");
-    }
-    
+public final class Darcsden extends DarcsRepositoryBrowser {
     @Extension
     public static class DescriptorImpl extends Descriptor<RepositoryBrowser<?>> {
-
-        @Override
         public String getDisplayName() {
             return "Darcsden";
         }
-
-        public FormValidation doCheck(@QueryParameter final String value) throws IOException, ServletException {
-            return new FormValidation.URLCheck() {
-                @Override
-                protected FormValidation check() throws IOException, ServletException {
-//                    String url = Util.fixEmpty(value);
-//
-//                    if (url == null) {
-//                        return FormValidation.ok();
-//                    }
-//
-//                    if (!url.endsWith("/")) {
-//                        url += '/';
-//                    }
-//
-//                    if (!URL_PATTERN.matcher(url).matches()) {
-//                        return FormValidation.errorWithMarkup("The URL should end like <tt>.../browse/foobar/</tt>");
-//                    }
-//
-//                    try {
-//                        if (!findText(open(new URL(url)), "FishEye")) {
-//                            return FormValidation.error("This is a valid URL but it doesn't look like FishEye");
-//                        }
-//                    } catch (IOException e) {
-//                        handleIOException(url, e);
-//                    }
-
-                    return FormValidation.ok();
-                }
-            }.check();
-        }
-
-        @Override
-        public Darcsden newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-            return req.bindParameters(Darcsden.class, "darcsden.darcs.");
-        }
-
-        private static final Pattern URL_PATTERN = Pattern.compile("http://darcsden.com/");
     }
 
+
     private static final long serialVersionUID = 1L;
+
+    public final URL url;
+
+	@DataBoundConstructor
+    public Darcsden(URL url) throws MalformedURLException {
+        this.url = normalizeToEndWithSlash(url);
+    }
+
+    public URL getChangeSetLink(DarcsChangeSet changeSet) throws IOException {
+        return new URL("");
+    }
+
+
+    public URL getFileLink(DarcsChangeSet.Path path) throws IOException {
+		return new URL("");
+    }
 }
