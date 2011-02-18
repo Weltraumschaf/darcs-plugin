5121c85772f434293e6f10d8ea0ae901ed855d3a
Weltraumschaf <ich@weltraumschaf.de>
2011-02-18 10:52:09 +0100

    Codeformatting.

--- src/main/java/org/jenkinsci/plugins/darcs/DarcsRevisionState.java
+++ src/main/java/org/jenkinsci/plugins/darcs/DarcsRevisionState.java
@@ -7,7 +7,6 @@
  * this stuff. If we meet some day, and you think this stuff is worth it,
  * you can buy me a beer in return.
  */
-
 package org.jenkinsci.plugins.darcs;
 
 import hudson.scm.SCMRevisionState;
@@ -71,22 +70,23 @@ import java.util.ArrayList;
  * @author Sven Strittmatter <ich@weltraumschaf.de>
  */
 public class DarcsRevisionState extends SCMRevisionState {
-	private List<String> hashes;
 
-	public DarcsRevisionState(ArrayList<String> hashes) {
-		super();
-		this.hashes = hashes;
-	}
-	
+    private List<String> hashes;
+
+    public DarcsRevisionState(ArrayList<String> hashes) {
+        super();
+        this.hashes = hashes;
+    }
+
     @Override
     public String toString() {
         return "RevisionState " + hashCode();
     }
-    
+
     @Override
     public boolean equals(Object other) {
         boolean result = false;
-        
+
         if (other instanceof DarcsRevisionState) {
             DarcsRevisionState that = (DarcsRevisionState) other;
             result = hashCode() == that.hashCode();
