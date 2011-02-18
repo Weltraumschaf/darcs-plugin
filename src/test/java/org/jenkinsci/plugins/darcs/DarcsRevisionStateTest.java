eff54494c923e4baa7aa08f389c0d904c6549d76
Weltraumschaf <ich@weltraumschaf.de>
2011-02-18 09:14:46 +0100

    Teststub for revision state tests.

---
+++ src/test/java/org/jenkinsci/plugins/darcs/DarcsRevisionStateTest.java
@@ -0,0 +1,23 @@
+/*
+ * LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * "Sven Strittmatter" <ich@weltraumschaf.de> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ */
+
+package org.jenkinsci.plugins.darcs;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Sven Strittmatter <ich@weltraumschaf.de>
+ */
+public class DarcsRevisionStateTest extends TestCase {
+    public DarcsRevisionStateTest(String testName) {
+        super(testName);
+    }
+}
