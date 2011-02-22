e45ea03976c77a23ac80da442aca15f36be50305
Weltraumschaf <ich@weltraumschaf.de>
2011-02-22 18:48:05 +0100

    Fixed tabs/spaces fuckup.

--- src/test/java/org/jenkinsci/plugins/darcs/DarcsChangeSetTest.java
+++ src/test/java/org/jenkinsci/plugins/darcs/DarcsChangeSetTest.java
@@ -7,7 +7,6 @@
  * this stuff. If we meet some day, and you think this stuff is worth it,
  * you can buy me a beer in return.
  */
-
 package org.jenkinsci.plugins.darcs;
 
 import junit.framework.Assert;
@@ -19,32 +18,28 @@ import org.junit.Ignore;
  * @author Sven Strittmatter <ich@weltraumschaf.de>
  */
 public class DarcsChangeSetTest extends TestCase {
+
     public DarcsChangeSetTest(String testName) {
         super(testName);
     }
 
     @Ignore("not ready yet")
     public void testGetAffectedPaths() {
-
     }
 
-	@Ignore("not ready yet")
-    public void testGetAddedPaths(){
+    @Ignore("not ready yet")
+    public void testGetAddedPaths() {
+    }
 
-	}
+    @Ignore("not ready yet")
+    public void testGetDeletedPaths() {
+    }
 
-	@Ignore("not ready yet")
-    public void testGetDeletedPaths(){
+    @Ignore("not ready yet")
+    public void testGetModifiedPaths() {
+    }
 
-	}
-
-	@Ignore("not ready yet")
-    public void testGetModifiedPaths(){
-
-	}
-
-	@Ignore("not ready yet")
-    public void testGetPaths(){
-
-	}
+    @Ignore("not ready yet")
+    public void testGetPaths() {
+    }
 }
