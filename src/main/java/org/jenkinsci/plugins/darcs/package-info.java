be0d6426bac9092b9ec439620262465afbb0405b
Weltraumschaf <ich@weltraumschaf.de>
2011-02-23 21:48:28 +0100

    Added package-info.java

---
+++ src/main/java/org/jenkinsci/plugins/darcs/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * "Sven Strittmatter" <ich@weltraumschaf.de> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ */
+
+/**
+ * This is the core package for the darcs jenkins plugin.
+ *
+ * @since 0.3.2
+ */
+package org.jenkinsci.plugins.darcs;
