6f45e74a27318cc5c081f00d4f0556515795827a
Weltraumschaf <ich@weltraumschaf.de>
2013-03-08 21:09:19 +0100

    Use earlier init milestone.

--- src/main/java/org/jenkinsci/plugins/darcs/DarcsScm.java
+++ src/main/java/org/jenkinsci/plugins/darcs/DarcsScm.java
@@ -442,7 +442,7 @@ public class DarcsScm extends SCM implements Serializable {
     /**
      * Add class name aliases for backward compatibility.
      */
-    @Initializer(before = InitMilestone.PLUGINS_STARTED)
+    @Initializer(before = InitMilestone.PLUGINS_LISTED)
     public static void addAliases() {
         // until version 0.3.6 the descriptor was inner class of DarcsScm
         Jenkins.XSTREAM2.addCompatibilityAlias("org.jenkinsci.plugins.darcs.DarcsScm$DescriptorImpl",
