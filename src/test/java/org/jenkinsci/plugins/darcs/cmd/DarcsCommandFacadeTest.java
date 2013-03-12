ea245aeb13f3901bbad56078c4cda2e3f538069f
Weltraumschaf <ich@weltraumschaf.de>
2013-03-12 23:56:27 +0100

    Extract repo fixture in tmp dir.

--- src/test/java/org/jenkinsci/plugins/darcs/cmd/DarcsCommandFacadeTest.java
+++ src/test/java/org/jenkinsci/plugins/darcs/cmd/DarcsCommandFacadeTest.java
@@ -11,30 +11,55 @@
 package org.jenkinsci.plugins.darcs.cmd;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver;
+import org.codehaus.plexus.logging.Logger;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import static org.mockito.Mockito.mock;
 
 /**
  *
  * @author Sven Strittmatter <ich@weltraumschaf.de>
  */
 public class DarcsCommandFacadeTest {
+    private static final String REPO_ARCHIVE = "/org/jenkinsci/plugins/darcs/cmd/repo.tar.gz";
+    private static final String REPO_DIR_NAME = "repo";
 
-    @Rule public ExpectedException thrown = ExpectedException.none();
     @Rule public TemporaryFolder folder= new TemporaryFolder();
 
-    private File prepareTestRepo() {
-        File tmpDir = folder.getRoot();
-        return tmpDir;
+    private File repo;
+
+    @Before
+    public void loadRepoArchive() throws URISyntaxException {
+        final URI resource = getClass().getResource(REPO_ARCHIVE).toURI();
+        repo = new File(resource);
+        assertThat(repo.exists(), is(true));
+    }
+
+    private File prepareTestRepo() throws URISyntaxException {
+        final File tmpDir = folder.getRoot();
+        final TarGZipUnArchiver ua = new TarGZipUnArchiver();
+        ua.enableLogging(mock(Logger.class));
+        ua.setSourceFile(repo);
+        ua.setDestDirectory(tmpDir);
+        ua.extract();
+        return new File(tmpDir, REPO_DIR_NAME);
     }
 
     @Test
     @Ignore("not ready yet")
-    public void lastSummarizedChanges() {
+    public void lastSummarizedChanges() throws URISyntaxException {
         // TODO Implement test
+        final File repo = prepareTestRepo();
+        assertThat(repo, is(notNullValue()));
     }
 
     @Test
